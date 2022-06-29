312bfef66ba2f09e8448bfe1ef38788307d724eb
enantes <sambaeverything@gmail.com>
2022-06-29 11:26:29 +0200

    Sam added comment on line 8

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ import __dirname from "./dirname.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import logger from "morgan";
-
+//Comment in Sam's branch
 import router from "./routes/lessons.js";
 
 const app = express();
